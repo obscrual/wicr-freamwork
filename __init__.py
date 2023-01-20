270608ae10249c77e62e51331c9aaef957e8c289
obscrual <77045082+obscrual@users.noreply.github.com>
2023-01-20 12:33:37 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 import os
 try:
     os.system("pip install requests")
+    input("")
 except:
     try:
         os.system("python -m pip install requests")
+        input("")
     except:
         print("libraries can't downloading \nplase add python path or another problem")
         input("")
